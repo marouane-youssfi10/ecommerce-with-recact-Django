688a7688cfd201943bdf05b0bab6ceda4f192193
marouane youssfi <marouaneyoussfi5@gmail.com>
2022-01-27 23:05:12 +0100

    update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducers } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 
 const reducer = combineReducers({
-    productList: productListReducers
+    productList: productListReducer, 
+    productDetails: productDetailsReducer,
 })
 
 const initialState = {}
 const middleware = [thunk]
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(reducer, initialState,
+     composeWithDevTools(applyMiddleware(...middleware)))
 
 export default store;
