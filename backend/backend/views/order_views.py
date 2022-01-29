11628023ccccc24a58ad392d610a4aceade91913
marouane youssfi <marouaneyoussfi5@gmail.com>
2022-01-29 22:05:30 +0100

    update

--- backend/backend/views/order_views.py
+++ backend/backend/views/order_views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from django.http import JsonResponse
 from .products import products
 from .models import *
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
@@ -48,23 +47,3 @@ def registerUser(request):
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-@permission_classes([IsAdminUser])
-def getUsers(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
