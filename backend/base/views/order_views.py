fc0a50106f096fbf845fc11764637926395b5dcf
marouane youssfi <marouaneyoussfi5@gmail.com>
2022-02-01 15:23:52 +0100

    update

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from base.models import Product, Order, OrderItem, ShippingAddress
-from base.serializers import ProductSerializer
+from base.serializers import ProductSerializer, OrderSerializer
 
 from rest_framework import status
 
@@ -26,8 +26,26 @@ def addOrderItems(request):
             totalPrice=data['totalPrice'],
         )
         # (2) Create ShippingAddress
-        
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
+        )
         # (3) Create Order items add set order to order relationship
-        # (3) Update stock
-        pass
-    return Response('ORDER')
+        for i in orderItems:
+            product = Product.objects.get(_id=i['product'])
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url
+            )
+            # (4) Update stock
+            product.countInStock -= item.qty
+            product.save()
+    serializer = OrderSerializer(Order, many=False)
+    return Response(serializer.data)
