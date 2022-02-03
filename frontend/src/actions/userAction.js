4d586eec96de5e87c43b68694a07e13b8b7899e6
marouane youssfi <marouaneyoussfi5@gmail.com>
2022-02-03 20:44:50 +0100

    update

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -29,10 +29,10 @@ import {
     USER_DELETE_SUCCESS,
     USER_DELETE_FAIL,
 
-    // USER_UPDATE_REQUEST,
-    // USER_UPDATE_SUCCESS,
-    // USER_UPDATE_FAIL,
-    // USER_UPDATE_RESET,
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL,
+    USER_UPDATE_RESET,
 
 } from '../constants/userConstants';
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
@@ -290,45 +290,45 @@ export const deleteUser = (id) => async (dispatch, getState) => {
 }
 
 
-// export const updateUser = (user) => async (dispatch, getState) => {
-//     try {
-//         dispatch({
-//             type: USER_UPDATE_REQUEST
-//         })
+export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_REQUEST
+        })
 
-//         const {
-//             userLogin: { userInfo },
-//         } = getState()
+        const {
+            userLogin: { userInfo },
+        } = getState()
 
-//         const config = {
-//             headers: {
-//                 'Content-type': 'application/json',
-//                 Authorization: `Bearer ${userInfo.token}`
-//             }
-//         }
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
 
-//         const { data } = await axios.put(
-//             `/api/users/update/${user._id}/`,
-//             user,
-//             config
-//         )
+        const { data } = await axios.put(
+            `/api/users/update/${user._id}/`,
+            user,
+            config
+        )
 
-//         dispatch({
-//             type: USER_UPDATE_SUCCESS,
-//         })
+        dispatch({
+            type: USER_UPDATE_SUCCESS,
+        })
 
-//         dispatch({
-//             type: USER_DETAILS_SUCCESS,
-//             payload: data
-//         })
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data
+        })
 
 
-//     } catch (error) {
-//         dispatch({
-//             type: USER_UPDATE_FAIL,
-//             payload: error.response && error.response.data.detail
-//                 ? error.response.data.detail
-//                 : error.message,
-//         })
-//     }
-// }
+    } catch (error) {
+        dispatch({
+            type: USER_UPDATE_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
